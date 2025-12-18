fe703ab9c4606ab8d51b50887648abcff9507ae1
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2025-12-18 16:07:55 +0200

    Add product category sections with "Load more" functionality

--- soundscape/src/pages/Products.tsx
+++ soundscape/src/pages/Products.tsx
@@ -51,26 +51,134 @@ export default function Products(){
     return(
         <>
         <Header />
-        <section  className="grid  grid-cols-3 place-items-center gap-y-18 my-38">
+        {/* headphones section */}
+        <section className="mt-28 ">
+        <div className="flex items-center gap-4 my-4 justify-end mr-10">
+        <div className="text-2xl">HeadPhones</div>
+        <button className="p-2 px-4 focus:bg-[#D3D3D3]  border border-[#B1A7A6] cursor-pointer">Load more</button>
+
+        </div>
+        <section  className="flex flex-row justify-end gap-10  mx-10">
         {storeItem.length > 0 && 
         storeItem.map((item: ProductInformation,index) => 
-            <section key={index} className="flex flex-col cursor-pointer justify-between p-4 text-[#2a2a2b] border border-[#B1A7A6] bg-[#F5F3F4] hover:bg-[#D3D3D3] hover:scale-101 transition delay-150 duration-300 hover:text-[#0B090A] h-144 w-max">
+            <section key={index} className="flex flex-col cursor-pointer justify-between p-4 text-[#2a2a2b] border border-[#B1A7A6] bg-[#F5F3F4] hover:bg-[#D3D3D3] hover:scale-101 transition delay-150 duration-300 hover:text-[#0B090A] h-90 w-max">
               <Link to="/SingleProduct" state={item}  >
-              <div className="bg-white border border-[#B1A7A6] w-100 h-90 max-h-90">
+              <div className="bg-white border border-[#B1A7A6] w-50 h-60 max-h-90">
                 {item.image && item.image.length > 0 && <img className="object-cover" src={item.image[0]} alt={item.name} />}
               </div>
-              <h1 className="text-4xl w-90 line-clamp-2 mb-4">{item.name}</h1>
-              <p className="text-[#161A1D] mb-4">{item.brand}</p>
+              <h1 className="text-2xl w-50 line-clamp-2">{item.name}</h1>
+              <p className="text-[#161A1D]">{item.brand}</p>
               <p className="border rounded-full bg-amber-400 w-4 h-4"></p>
-              <h1 className="text-3xl  mt-4">R {item.price}</h1>
+              <h1 className="text-xl  ">R {item.price}</h1>
            </Link>
             </section>
 
-          )}
+          ).slice(0,6)}
           </section>
-          <section className="flex justify-center m-8">
-            <button className="p-4 px-8 focus:bg-[#D3D3D3]  border border-[#B1A7A6] cursor-pointer">Load more</button>
+</section>
+
+        {/* speakers section */}
+        <section className="mt-28 mb-10">
+        <div className="flex items-center gap-4 my-4 justify-end mr-10">
+        <div className="text-2xl">Speakers</div>
+        <button className="p-2 px-4 focus:bg-[#D3D3D3]  border border-[#B1A7A6] cursor-pointer">Load more</button>
+
+        </div>
+        <section  className="flex flex-row justify-end gap-10  mx-10">
+        {storeItem.length > 0 && 
+        storeItem.map((item: ProductInformation,index) => 
+            <section key={index} className="flex flex-col cursor-pointer justify-between p-4 text-[#2a2a2b] border border-[#B1A7A6] bg-[#F5F3F4] hover:bg-[#D3D3D3] hover:scale-101 transition delay-150 duration-300 hover:text-[#0B090A] h-90 w-max">
+              <Link to="/SingleProduct" state={item}  >
+              <div className="bg-white border border-[#B1A7A6] w-50 h-60 max-h-90">
+                {item.image && item.image.length > 0 && <img className="object-cover" src={item.image[0]} alt={item.name} />}
+              </div>
+              <h1 className="text-2xl w-50 line-clamp-2">{item.name}</h1>
+              <p className="text-[#161A1D]">{item.brand}</p>
+              <p className="border rounded-full bg-amber-400 w-4 h-4"></p>
+              <h1 className="text-xl  ">R {item.price}</h1>
+           </Link>
+            </section>
+
+          ).slice(0,6)}
           </section>
+</section>
+
+        {/* Earphones section */}
+        <section className="mt-28 ">
+        <div className="flex items-center gap-4 my-4 justify-end mr-10">
+        <div className="text-2xl">Earphones</div>
+        <button className="p-2 px-4 focus:bg-[#D3D3D3]  border border-[#B1A7A6] cursor-pointer">Load more</button>
+
+        </div>
+        <section  className="flex flex-row justify-end gap-10  mx-10">
+        {storeItem.length > 0 && 
+        storeItem.map((item: ProductInformation,index) => 
+            <section key={index} className="flex flex-col cursor-pointer justify-between p-4 text-[#2a2a2b] border border-[#B1A7A6] bg-[#F5F3F4] hover:bg-[#D3D3D3] hover:scale-101 transition delay-150 duration-300 hover:text-[#0B090A] h-90 w-max">
+              <Link to="/SingleProduct" state={item}  >
+              <div className="bg-white border border-[#B1A7A6] w-50 h-60 max-h-90">
+                {item.image && item.image.length > 0 && <img className="object-cover" src={item.image[0]} alt={item.name} />}
+              </div>
+              <h1 className="text-2xl w-50 line-clamp-2">{item.name}</h1>
+              <p className="text-[#161A1D]">{item.brand}</p>
+              <p className="border rounded-full bg-amber-400 w-4 h-4"></p>
+              <h1 className="text-xl  ">R {item.price}</h1>
+           </Link>
+            </section>
+
+          ).slice(0,6)}
+          </section>
+</section>
+        {/* Soundbars section */}
+        <section className="mt-28 ">
+        <div className="flex items-center gap-4 my-4 justify-end mr-10">
+        <div className="text-2xl">Soundbars</div>
+        <button className="p-2 px-4 focus:bg-[#D3D3D3]  border border-[#B1A7A6] cursor-pointer">Load more</button>
+
+        </div>
+        <section  className="flex flex-row justify-end gap-10  mx-10">
+        {storeItem.length > 0 && 
+        storeItem.map((item: ProductInformation,index) => 
+            <section key={index} className="flex flex-col cursor-pointer justify-between p-4 text-[#2a2a2b] border border-[#B1A7A6] bg-[#F5F3F4] hover:bg-[#D3D3D3] hover:scale-101 transition delay-150 duration-300 hover:text-[#0B090A] h-90 w-max">
+              <Link to="/SingleProduct" state={item}  >
+              <div className="bg-white border border-[#B1A7A6] w-50 h-60 max-h-90">
+                {item.image && item.image.length > 0 && <img className="object-cover" src={item.image[0]} alt={item.name} />}
+              </div>
+              <h1 className="text-2xl w-50 line-clamp-2">{item.name}</h1>
+              <p className="text-[#161A1D]">{item.brand}</p>
+              <p className="border rounded-full bg-amber-400 w-4 h-4"></p>
+              <h1 className="text-xl  ">R {item.price}</h1>
+           </Link>
+            </section>
+
+          ).slice(0,6)}
+          </section>
+</section>
+        {/* Instruments section */}
+        <section className="mt-28 mb-10">
+        <div className="flex items-center gap-4 my-4 justify-end mr-10">
+        <div className="text-2xl">Instruments</div>
+        <button className="p-2 px-4 focus:bg-[#D3D3D3]  border border-[#B1A7A6] cursor-pointer">Load more</button>
+
+        </div>
+        <section  className="flex flex-row justify-end gap-10  mx-10">
+        {storeItem.length > 0 && 
+        storeItem.map((item: ProductInformation,index) => 
+            <section key={index} className="flex flex-col cursor-pointer justify-between p-4 text-[#2a2a2b] border border-[#B1A7A6] bg-[#F5F3F4] hover:bg-[#D3D3D3] hover:scale-101 transition delay-150 duration-300 hover:text-[#0B090A] h-104 w-max">
+              <Link to="/SingleProduct" state={item}  >
+              <div className="bg-white border border-[#B1A7A6] w-50 h-70 max-h-90">
+                {item.image && item.image.length > 0 && <img className="object-cover" src={item.image[0]} alt={item.name} />}
+              </div>
+              <h1 className="text-2xl w-50 line-clamp-2">{item.name}</h1>
+              <p className="text-[#161A1D]">{item.brand}</p>
+              <p className="border rounded-full bg-amber-400 w-4 h-4"></p>
+              <h1 className="text-xl  ">R {item.price}</h1>
+           </Link>
+            </section>
+
+          ).slice(0,6)}
+          </section>
+</section>
+         
         <Footer />
         </>
     )
