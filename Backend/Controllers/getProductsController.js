367119e273031510e7f71eab4fa2056cc433abe8
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2026-01-05 12:48:17 +0200

    Feature: completed "get Products Controller" logic, Altered the expiration on the generated token

--- Backend/Controllers/getProductsController.js
+++ Backend/Controllers/getProductsController.js
@@ -1,3 +1,12 @@
-export default function getProductsController(req, res) {
-    res.send('Get Product Controller');
+import { client } from "../index.js";
+export default async function getProductsController(req, res) {
+    try {
+
+        const getProductsQuery = 'SELECT * FROM products';
+        const result = await client.query(getProductsQuery);
+        return res.status(200).json({ products: result.rows });
+    } catch (error) {
+        console.log("Error fetching products", error);
+        return res.json({ message: "Error fetching products", error: error.message });
+    }
 }
