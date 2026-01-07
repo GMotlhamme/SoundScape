78b553b6919d32c7cb264b979aa2da41b9383d2b
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2026-01-07 18:44:00 +0200

    Feat: Post products int the checkout table logic and view checkout table logic completed

--- Backend/Controllers/getUserCartController.js
+++ Backend/Controllers/getUserCartController.js
@@ -1,3 +1,12 @@
-export default function getUserCartController(req, res){
-    res.json({message: 'User cart retrieved successfully'});
+import { client } from "../index.js";
+export default async function getUserCartController(req, res){
+    try {
+        const user_id = req.user.id;
+        const cartQuery = 'SELECT * FROM checkout WHERE user_id = $1';
+        const cartItems = await client.query(cartQuery, [user_id]);
+        return res.status(200).json({ items: cartItems.rows });
+    } catch (error) {
+        console.error("Error retrieving user cart:", error);
+        return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
 }
--- Backend/Controllers/storeCartItemsController.js
+++ Backend/Controllers/storeCartItemsController.js
@@ -1,3 +1,19 @@
-export default function storeCartItemsController(req, res){
-    res.json({message: 'Cart items stored successfully'});
+import { client } from "../index.js";
+
+export default async function storeCartItemsController(req, res){
+    try {
+        const user_id = req.user.id; 
+        const { id } = req.body;
+        const product_id = id;
+        const created_at = new Date();
+        if (!id  || !user_id) {
+            return res.status(400).json({ message: "Missing required fields or not logged in" });
+        }
+        const productQuery = 'INSERT INTO checkout (user_id, product_id, created_at) VALUES ($1, $2, $3)';
+        await client.query(productQuery, [user_id, product_id, created_at]);
+        return res.status(200).json({ message: "Cart items stored successfully" });
+    } catch (error) {
+        console.error("Error storing cart items:", error);
+        return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
 }
