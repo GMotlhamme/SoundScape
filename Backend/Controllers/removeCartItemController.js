b1a1514e5c6b20b261af8073d128ef8e96a972f6
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2026-01-08 19:39:30 +0200

    feat: implement remove cart item functionality with error handling

--- Backend/Controllers/removeCartItemController.js
+++ Backend/Controllers/removeCartItemController.js
@@ -1,3 +1,14 @@
-export default function removeCartItemController(req, res){
-    res.json({message: 'Cart item removed successfully'});
+import { client } from "../index.js";
+export default async function removeCartItemController(req, res){
+    try {
+        const user_id = req.user && req.user.id
+        if (!user_id) return res.status(401).json({message: "Unauthorized procedure"})
+
+            const deleteCartQuery = 'DELETE * FROM checkout WHERE user_id = $1'
+            await client.query(deleteCartQuery, [user_id])
+            res.json({message:"Cart cleared"})
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({"Internal server error": error})
+    }
 }
