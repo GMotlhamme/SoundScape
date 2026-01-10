84fb41ab2f9d20b1e196778f1a09a478d00a9aaf
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2026-01-10 20:55:55 +0200

    feat: implement wishlist item storage with error handling and validation

--- Backend/Controllers/storeWishlistItemsController.js
+++ Backend/Controllers/storeWishlistItemsController.js
@@ -1,3 +1,17 @@
-export default function storeWishlistItemsController(req, res){
-    res.json({message: 'Wishlist items stored successfully'});
+export default async function storeWishlistItemsController(req, res){
+      try {
+            const user_id = req.user.id; 
+            const { id } = req.body;
+            const product_id = id;
+            const created_at = new Date();
+            if (!id  || !user_id) {
+                return res.status(400).json({ message: "Missing required fields or not logged in" });
+            }
+            const productQuery = 'INSERT INTO wishlist (user_id, product_id, created_at) VALUES ($1, $2, $3)';
+            await client.query(productQuery, [user_id, product_id, created_at]);
+            return res.status(200).json({ message: "Cart items stored successfully" });
+        } catch (error) {
+            console.error("Error storing cart items:", error);
+            return res.status(500).json({ message: "Internal server error", error: error.message });
+        }
 }
